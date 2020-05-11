79634292b876c59a0396033d38b5b84406b873a7
dongle <dongle@stunitas.com>
2020-05-11 19:42:07 +0900

    init

---
+++ buildkts/src/main/kotlin/com/devleop/buildkts/AppDependencies.kt
@@ -0,0 +1,16 @@
+package com.devleop.buildkts
+
+
+object Apps{
+    const val compileSdk = 29
+    const val minSdk = 21
+    const val target = 29
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+}
+
+object Versions{
+    const val gradle = "3.5.2"
+    const val kotlin = "1.3.50"
+    const val appCommpat = "1.0.2"
+}
